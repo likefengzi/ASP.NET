904720cf6abbd2108e34acb8dd94f678c8697b46
likefengzi <78586025+likefengzi@users.noreply.github.com>
2021-11-02 17:06:06 +0800

    Add files via upload

---
+++ Lab4.1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            SolidBrush brush = new SolidBrush(Color.Red);
+            g.FillRectangle(brush, new Rectangle(100,100,100,100));
+
+            Pen pen = new Pen(Color.Blue,10.0f);
+            g.DrawLines(pen, new Point[] { new Point(300, 100), new Point(500, 50),new Point(400,20), new Point(300, 100) });
+
+        }
+    }
+}
