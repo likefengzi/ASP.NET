ef81d0d23a6d7695b3cc82ce1e0e30788e33a11c
likefengzi <78586025+likefengzi@users.noreply.github.com>
2021-11-17 19:44:06 +0800

    Add files via upload

---
+++ Lab5.3/index.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lab5._3
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            String name = TextBox1.Text.Trim();
+            String password = TextBox2.Text.Trim();
+            if (name == "")
+            {
+                Response.Write("用户名不能为空");
+                TextBox1.Focus();
+                return;
+            }
+            else if (password == "")
+            {
+                Response.Write("密码不能为空");
+                TextBox2.Focus();
+                return;
+            }
+            else
+            {
+                if (name.Equals("张三") && password.Equals("123456"))
+                {
+                    Session["useInfo"] = "张三";
+                    Response.Redirect("view.aspx");
+                }
+                else
+                {
+                    Response.Write("用户名密码错误");
+                }
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Session.Abandon();
+            Session.Clear();
+        }
+    }
+}
---
+++ Lab5.3/view.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lab5._3
+{
+    public partial class view : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["useInfo"] == null)
+            {
+                Response.Write("<B><font color='red'>您不是合法用户</font></B>");
+                Response.Write("<P><A href='index.aspx'>进入主页面</A></P>");
+            }
+            else
+            {
+                Response.Write("<B>恭喜登录成功</B>");
+            }
+        }
+    }
+}
